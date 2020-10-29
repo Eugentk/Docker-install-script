5c23b960b3e0af7550d707faa1c9049a95fe1f1e
Eugen Tkachenko <EugenTkachenko@mail.com>
2020-10-29 12:53:03 +0200

    Changed version docker-compose

--- Autoinstalling_Docker.sh
+++ Autoinstalling_Docker.sh
@@ -13,7 +13,7 @@ sudo apt-get update
 sudo apt-get -y install docker-ce
 
 # Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod a+x /usr/local/bin/docker-compose
 # Start docker
 sudo systemctl enable docker
